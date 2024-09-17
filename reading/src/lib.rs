2c5d73b02b17807ea63e4e4e4834f779f9095272
Charles Eckman <charles@cceckman.com>
2024-09-17 10:51:35 -0400

    Style reading list interface

--- reading/src/lib.rs
+++ reading/src/lib.rs
@@ -2,7 +2,7 @@ use std::sync::{Arc, Mutex};
 
 use axum::{
     extract::{Path, State},
-    http::StatusCode,
+    http::{header::CONTENT_TYPE, StatusCode},
     response::IntoResponse,
     routing::get,
 };
@@ -20,7 +20,9 @@ pub fn serve(db: &std::path::Path) -> Result<axum::Router, Error> {
     let conn = Connection::open(db)?;
     let s = Arc::new(Mutex::new(Server { conn }));
     Ok(axum::Router::new()
-        .route("/roundup/:date/", get(render_roundup))
+        .route("/roundups/:date/", get(render_roundup))
+        .route("/articles/:id/", get(render_article))
+        .route("/style.css", get(css))
         .with_state(s))
 }
 
@@ -35,21 +37,34 @@ struct RoundupRow {
     entry: ReadingListEntry,
 }
 
-fn destruct_row(row: &rusqlite::Row) -> rusqlite::Result<RoundupRow> {
+fn destruct_roundup_row(row: &rusqlite::Row) -> rusqlite::Result<RoundupRow> {
     let html = markdown::to_html(&row.get::<_, String>("body_text")?);
     let included = row.get::<_, Option<bool>>("included")?.unwrap_or(false);
     Ok(RoundupRow {
         id: row.get("id")?,
         included,
         html,
-        entry: ReadingListEntry {
-            url: row.get::<_, String>("url")?.parse().unwrap(),
-            source_date: row.get::<_, String>("source_date")?.parse().unwrap(),
-            original_text: row.get::<_, String>("original_text")?.to_owned(),
-            body_text: row.get::<_, String>("body_text")?.to_owned(),
-            read: row.get("read")?,
-        },
+        entry: destruct_entry(row)?,
     })
+}
+
+fn destruct_entry(row: &rusqlite::Row) -> rusqlite::Result<ReadingListEntry> {
+    Ok(ReadingListEntry {
+        url: row.get::<_, String>("url")?.parse().unwrap(),
+        source_date: row.get::<_, String>("source_date")?.parse().unwrap(),
+        original_text: row.get::<_, String>("original_text")?.to_owned(),
+        body_text: row.get::<_, String>("body_text")?.to_owned(),
+        read: row.get("read")?,
+    })
+}
+
+async fn css() -> impl IntoResponse {
+    static CSS_CONTENT: &str = include_str!("style.css");
+    (
+        StatusCode::OK,
+        [(CONTENT_TYPE, "text/css")],
+        CSS_CONTENT.to_owned(),
+    )
 }
 
 /// Render the editor for a roundup post.
@@ -80,7 +95,72 @@ async fn render_roundup(
     }
 }
 
+/// Render the editor for a roundup post.
+async fn render_article(
+    State(server): State<Arc<Mutex<Server>>>,
+    Path(p): Path<isize>,
+    // TODO: support "get as YAML" too
+) -> impl IntoResponse {
+    let mut server = server.lock().unwrap();
+    match server.render_article(p) {
+        Ok(v) => v.into_response(),
+        Err(e) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("unexpected error: {e}"),
+        )
+            .into_response(),
+    }
+}
+
 impl Server {
+    fn render_article(
+        &mut self,
+        id: isize,
+        // TODO: support "get as YAML" too
+    ) -> Result<impl IntoResponse, Error> {
+        // Query everything, prioritizing stuff in the roundup.
+        let (count, entry) = self
+            .conn
+            .prepare(
+                r#"
+            SELECT *, COUNT(roundup_contents.date) as roundups
+            FROM reading_list
+            LEFT JOIN roundup_contents ON reading_list.id = roundup_contents.entry
+            WHERE reading_list.id = :id
+            "#,
+            )?
+            .query_row(named_params! {":id": id}, |row| {
+                let count: isize = row.get("roundups")?;
+                let entry = destruct_entry(row)?;
+                Ok((count, entry))
+            })?;
+        Ok(maud::html! {
+            head { link rel="stylesheet" href="/style.css"; }
+            body { main {
+                div class="summary" {
+                    h3 {
+                        a href=(entry.url) { (entry.url) }
+                    }
+                    h4 class="tile-title" {
+                        p { (entry.source_date) }
+                        p { (format!("{count} roundups")) }
+                    }
+                }
+                form {
+                    button label="Save" action="submit" { "Save" }
+                    details { summary { "Original" } pre { (entry.original_text) } }
+                    details open {
+                        summary { "Preview" }
+                        div class="summary" {
+                            (maud::PreEscaped(markdown::to_html(&entry.body_text)))
+                        }
+                    }
+                    textarea { (entry.body_text) }
+                }
+            } }
+        })
+    }
+
     fn render_roundup(
         &mut self,
         date: NaiveDate,
@@ -99,20 +179,40 @@ impl Server {
             ORDER BY included DESC, source_date DESC
             "#,
             )?
-            .query_map(named_params! {":date": format!("{date}")}, destruct_row)?
+            .query_map(
+                named_params! {":date": format!("{date}")},
+                destruct_roundup_row,
+            )?
             .collect();
         let rows = rows?;
+        let included_rows = rows.iter().filter(|v| v.included);
+        let excluded_rows = rows.iter().filter(|v| !v.included);
 
         Ok(maud::html! {
-            h1 { (format!("Reading Roundup, {date}")) }
-            form { table {
-                @for row in rows { tr {
-                    td { input type="checkbox" name=(format!("included-{}", row.id)) checked?[row.included] ; }
-                    td { (format!("{}", row.entry.source_date)) }
-                    td { a href=(format!("/article/{}/", row.id)) { (maud::PreEscaped("&nbsp;🖉&nbsp;")) } }
-                    td { (maud::PreEscaped(row.html)) }
-                } }
-            } }
+            head { link rel="stylesheet" href="/style.css"; }
+            body {
+                div class="article-meta" { h1 { "Reading Roundup" } }
+                main {
+                    div class="summary" {
+                        h3 class="tile-title" { a { (format!("Reading Roundup, {date}")) } a { "Save" } }
+                    table {
+                    @for row in included_rows { tr {
+                        td { (maud::PreEscaped(row.html.clone())) }
+                        td { input type="checkbox" name=(format!("included-{}", row.id)) checked?[row.included] ; }
+                        td { (format!("{}", row.entry.source_date)) }
+                        td { a href=(format!("/articles/{}/", row.id)) { (maud::PreEscaped("&nbsp;🖉&nbsp;")) } }
+                    } }
+                    } }
+
+                    h3 { "Add to this roundup: " }
+                    table { @for row in excluded_rows { tr {
+                        td { input type="checkbox" name=(format!("included-{}", row.id)) checked?[row.included] ; }
+                        td { (format!("{}", row.entry.source_date)) }
+                        td { a href=(format!("/articles/{}/", row.id)) { (maud::PreEscaped("&nbsp;🖉&nbsp;")) } }
+                        td { (maud::PreEscaped(row.html.clone())) }
+                    } } }
+                }
+            }
         })
     }
 }
